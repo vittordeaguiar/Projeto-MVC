144548bbbed47fc43049760c3cdab1d00c2bdc69
Vittor de Aguiar <103087952+vittoraguiar@users.noreply.github.com>
2022-09-13 18:53:46 -0300

    atualizando projeto

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto_MVC.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
